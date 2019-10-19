e8bc8881b3cc8729ae04aebccdeee7a50c09df67
Michael Beamish <michael.beamish@microchip.com>
2019-10-19 14:18:35 -0700

    Added image.

--- client-app/src/views/Home.vue
+++ client-app/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <img alt="Vue logo" src="../assets/images/giphy.gif" />
+    <HelloWorld msg="We're in business now, baby!" />
   </div>
 </template>
 
