796bd963e5a46488d51127c7c627381213cbf66c
Derek Bixler <derek.bixler@trojans.dsu.edu>
2019-10-19 18:16:03 -0700

    strip session id

--- Web.Api/controllers/payment_controller.js
+++ Web.Api/controllers/payment_controller.js
@@ -1,19 +1,15 @@
 const knex = require("../db/knex.js");
 
 module.exports = {
-  stripe: function(req, res) {
+  stripe_get_id: function(req, res) {
     console.log("" + req.body.name);
+    var sessionJSON;
+    var id;
     const stripe = require('stripe')('sk_test_3KNveg5tCJaQPS1NcCSWohFg00WlCKYVhJ');
     (async () => {
       const session = await stripe.checkout.sessions.create({
         payment_method_types: ['card'],
         line_items: [{
-          // name: 'T-shirt',
-          // description: 'Comfortable cotton t-shirt',
-          // images: ['https://example.com/t-shirt.png'],
-          // amount: 500,
-          // currency: 'usd',
-          // quantity: 1,
           name: req.body.name,
           description: req.body.description,
           amount: req.body.amount,
@@ -23,24 +19,9 @@ module.exports = {
         success_url: 'https://example.com/success',
         cancel_url: 'https://example.com/cancel',
       });
-      // res.json(session);
-      console.log(""+ session.id);
+      id = session.id;
+      console.log(session);
+      res.send(id);
     })();
-    // res.
-
-    // console.log("" + session.id);
-    res.send();
-
-    // stripe.redirectToCheckout({
-    //     // Make the id field from the Checkout Session creation API response
-    //     // available to this file, so you can provide it as parameter here
-    //     // instead of the {{CHECKOUT_SESSION_ID}} placeholder.
-    //     sessionId: '{{CHECKOUT_SESSION_ID}}'
-    //   }).then(function (result) {
-    //     // If `redirectToCheckout` fails due to a browser or network
-    //     // error, display the localized error message to your customer
-    //     // using `result.error.message`.
-    //   });
-
   }
 }
